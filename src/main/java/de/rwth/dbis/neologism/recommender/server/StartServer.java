59dc9eca325c0927b6d62944d38ec3a558137d7f
michaelcochez <michael.cochez@>
2018-03-09 21:39:45 +0100

    Cleaning up and adding the code for starting with Jetty and Local vocabulary loading

---
+++ src/main/java/de/rwth/dbis/neologism/recommender/server/StartServer.java
@@ -0,0 +1,34 @@
+package de.rwth.dbis.neologism.recommender.server;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import uni.rwth.neolog.recommeder.rest.RecommendVocabulary;
+
+public class StartServer {
+	public static void main(String[] args) throws Exception {
+		
+		
+		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
+		context.setContextPath("/");		
+
+				
+		ServletHolder jerseyServlet = context.addServlet(org.glassfish.jersey.servlet.ServletContainer.class, "/*");
+		
+		jerseyServlet.setInitOrder(0);
+		jerseyServlet.setInitParameter("jersey.config.server.provider.classnames", RecommendVocabulary.class.getCanonicalName());
+
+		
+		Server jettyServer = new Server(8080);
+		jettyServer.setHandler(context);
+
+		
+		try {
+			jettyServer.start();
+			jettyServer.join();
+		} finally {
+			//jettyServer.destroy();
+		}
+	}
+}
