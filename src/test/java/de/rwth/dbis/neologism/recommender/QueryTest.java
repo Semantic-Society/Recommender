db3aabd9db7ef4472a9120217d52345958560db5
Johannes Lipp <johannes.lipp@fit.fraunhofer.de>
2020-07-02 16:12:51 +0200

    Refactor more

--- src/test/java/de/rwth/dbis/neologism/recommender/QueryTest.java
+++ src/test/java/de/rwth/dbis/neologism/recommender/QueryTest.java
@@ -1,178 +1,175 @@
 package de.rwth.dbis.neologism.recommender;
 
-import java.io.StringReader;
-import java.util.Collections;
-
+import com.google.common.collect.Lists;
+import com.google.common.collect.Sets;
+import junit.framework.Assert;
+import junit.framework.TestCase;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
-
-import junit.framework.Assert;
-import junit.framework.TestCase;
+import java.io.StringReader;
+import java.util.Collections;
 
 public class QueryTest extends TestCase {
 
-	private static Model simpleModel() {
-		Model model = (Model) ModelFactory.createDefaultModel();
-		String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
-				+ "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
-				+ "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
-				+ "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#G> .\n"
-				+ "<http://ex.com#A> <http://ex.com#P1> <neo://query/bla> .\n";
+    private static Model simpleModel() {
+        Model model = ModelFactory.createDefaultModel();
+        String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
+                + "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
+                + "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
+                + "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#G> .\n"
+                + "<http://ex.com#A> <http://ex.com#P1> <neo://query/bla> .\n";
 
-		StringReader r = new StringReader(data);
+        StringReader r = new StringReader(data);
 
-		model = model.read(r, null, "N-TRIPLE");
-		return model;
-	}
+        model = model.read(r, null, "N-TRIPLE");
+        return model;
+    }
 
-	private static Model modelWithOneClass() {
-		Model model = (Model) ModelFactory.createDefaultModel();
-		String data = "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> ."
-				+ "<http://ex.com#A> <http://ex.com#P1> <neo://query/bla> .\n";
-		StringReader r = new StringReader(data);
-		model = model.read(r, null, "N-TRIPLE");
-		return model;
-	}
+    private static Model modelWithOneClass() {
+        Model model = ModelFactory.createDefaultModel();
+        String data = "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> ."
+                + "<http://ex.com#A> <http://ex.com#P1> <neo://query/bla> .\n";
+        StringReader r = new StringReader(data);
+        model = model.read(r, null, "N-TRIPLE");
+        return model;
+    }
 
-	private static Model modelWithClasses() {
-		Model model = (Model) ModelFactory.createDefaultModel();
-		String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
-				+ "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
-				+ "<http://ex.com#B> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
-				+ "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
-				+ "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
-				+ "<http://ex.com#A> <http://ex.com#P1> <neo://query/bla> .\n";
-		StringReader r = new StringReader(data);
-		model = model.read(r, null, "N-TRIPLE");
-		return model;
-	}
+    private static Model modelWithClasses() {
+        Model model = ModelFactory.createDefaultModel();
+        String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
+                + "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
+                + "<http://ex.com#B> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
+                + "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
+                + "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
+                + "<http://ex.com#A> <http://ex.com#P1> <neo://query/bla> .\n";
+        StringReader r = new StringReader(data);
+        model = model.read(r, null, "N-TRIPLE");
+        return model;
+    }
 
-	public void testQueryModelStringInt_SimpleParts() {
-		Model m = simpleModel();
-		Query q = new Query(m, 20);
-		Assert.assertEquals(20, q.limit);
-		Assert.assertEquals(m, q.context);
-		assertEquals("bla", q.queryString);
-	}
+    private static Model modelWithClassesPermuted() {
+        Model model = ModelFactory.createDefaultModel();
+        String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
+                + "<http://ex.com#B> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
+                + "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
+                + "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
+                + "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
+                + "<http://ex.com#A> <http://ex.com#P1> <neo://query/bla> .\n";
+        StringReader r = new StringReader(data);
+        model = model.read(r, null, "N-TRIPLE");
+        return model;
+    }
 
-	public void testQueryModelStringInt_ClassesEmpty() {
-		Model m = simpleModel();
-		Query q = new Query(m, 20);
-		assertEquals("bla", q.queryString);
-		assertEquals(q.getLocalClassNames(), Collections.emptyList());
-	}
+    private static Model modelWithClassesAndQuery() {
+        Model model = ModelFactory.createDefaultModel();
+        String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
+                + "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
+                + "<http://ex.com#B> <http://ex.com#P1> <neo://query/bla> .\n"
+                + "<http://ex.com#B> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
+                + "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
+                + "<neo://query/bla> <http://ex.com#P1> <http://ex.com#B> .\n"
+                + "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n";
+        StringReader r = new StringReader(data);
+        model = model.read(r, null, "N-TRIPLE");
+        return model;
+    }
 
-	public void testQueryModelStringInt_SingleClass() {
-		Model m = modelWithOneClass();
-		Query q = new Query(m, 20);
-		assertEquals(Lists.newArrayList("A"), q.getLocalClassNames());
-	}
+    private static Model modelWithClassesAndThreeQueries() {
+        Model model = ModelFactory.createDefaultModel();
+        String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
+                + "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
+                + "<http://ex.com#B> <http://ex.com#P1> <neo://query/bla> .\n"
+                + "<http://ex.com#B> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
+                + "<http://ex.com#B> <http://ex.com#P1> <neo://query/blo> .\n"
+                + "<neo://query/blu> <http://ex.com#P1> <http://ex.com#B> .\n"
+                + "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
+                + "<neo://query/bla> <http://ex.com#P1> <http://ex.com#B> .\n"
+                + "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n";
+        StringReader r = new StringReader(data);
+        model = model.read(r, null, "N-TRIPLE");
+        return model;
+    }
 
-	public void testQueryModelStringInt_Classes() {
-		Model m = modelWithClasses();
-		Query q = new Query(m, 20);
-		assertEquals(Sets.newHashSet("A", "B"), Sets.newHashSet(q.getLocalClassNames()));
-	}
+    public void testQueryModelStringInt_SimpleParts() {
+        Model m = simpleModel();
+        Query q = new Query(m, 20);
+        Assert.assertEquals(20, q.limit);
+        Assert.assertEquals(m, q.context);
+        assertEquals("bla", q.queryString);
+    }
 
-	private static Model modelWithClassesPermuted() {
-		Model model = (Model) ModelFactory.createDefaultModel();
-		String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
-				+ "<http://ex.com#B> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
-				+ "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
-				+ "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
-				+ "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
-				+ "<http://ex.com#A> <http://ex.com#P1> <neo://query/bla> .\n";
-		StringReader r = new StringReader(data);
-		model = model.read(r, null, "N-TRIPLE");
-		return model;
-	}
+    public void testQueryModelStringInt_ClassesEmpty() {
+        Model m = simpleModel();
+        Query q = new Query(m, 20);
+        assertEquals("bla", q.queryString);
+        assertEquals(q.getLocalClassNames(), Collections.emptyList());
+    }
 
-	public void testQueryModelStringInt_HashForPermutations() {
-		Model m1 = modelWithClasses();
-		Query q1 = new Query(m1, 20);
-		Model m2 = modelWithClassesPermuted();
-		Query q2 = new Query(m2, 20);
-		assertTrue("The hash shoulf not be modified by permutations.", q1.getContextHash().equals(q2.getContextHash()));
-	}
+    public void testQueryModelStringInt_SingleClass() {
+        Model m = modelWithOneClass();
+        Query q = new Query(m, 20);
+        assertEquals(Lists.newArrayList("A"), q.getLocalClassNames());
+    }
 
-	private static Model modelWithClassesAndQuery() {
-		Model model = (Model) ModelFactory.createDefaultModel();
-		String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
-				+ "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
-				+ "<http://ex.com#B> <http://ex.com#P1> <neo://query/bla> .\n"
-				+ "<http://ex.com#B> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
-				+ "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
-				+ "<neo://query/bla> <http://ex.com#P1> <http://ex.com#B> .\n"
-				+ "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n";
-		StringReader r = new StringReader(data);
-		model = model.read(r, null, "N-TRIPLE");
-		return model;
-	}
+    public void testQueryModelStringInt_Classes() {
+        Model m = modelWithClasses();
+        Query q = new Query(m, 20);
+        assertEquals(Sets.newHashSet("A", "B"), Sets.newHashSet(q.getLocalClassNames()));
+    }
 
-	public void testQueryModelStringInt_ClassesAndQuery() {
-		Model m = modelWithClassesAndQuery();
-		Query q = new Query(m, 20);
-		assertEquals("The queries should be ignored when finding the classes from the context.",
-				Sets.newHashSet("A", "B"), Sets.newHashSet(q.getLocalClassNames()));
-	}
+    public void testQueryModelStringInt_HashForPermutations() {
+        Model m1 = modelWithClasses();
+        Query q1 = new Query(m1, 20);
+        Model m2 = modelWithClassesPermuted();
+        Query q2 = new Query(m2, 20);
+        assertEquals("The hash shoulf not be modified by permutations.", q1.getContextHash(), q2.getContextHash());
+    }
 
-	public void testQueryModelStringInt_Query() {
-		Model m = modelWithClassesAndQuery();
-		Query q = new Query(m, 20);
-		assertEquals("The queries should be ignored when finding the classes from the context.", "bla", q.queryString);
-	}
+    public void testQueryModelStringInt_ClassesAndQuery() {
+        Model m = modelWithClassesAndQuery();
+        Query q = new Query(m, 20);
+        assertEquals("The queries should be ignored when finding the classes from the context.",
+                Sets.newHashSet("A", "B"), Sets.newHashSet(q.getLocalClassNames()));
+    }
 
-	private static Model modelWithClassesAndThreeQueries() {
-		Model model = (Model) ModelFactory.createDefaultModel();
-		String data = "<http://ex.com#A> <http://ex.com#P1> <http://ex.com#B> .\n"
-				+ "<http://ex.com#B> <http://ex.com#P1> <http://ex.com#C> .\n"
-				+ "<http://ex.com#B> <http://ex.com#P1> <neo://query/bla> .\n"
-				+ "<http://ex.com#B> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n"
-				+ "<http://ex.com#B> <http://ex.com#P1> <neo://query/blo> .\n"
-				+ "<neo://query/blu> <http://ex.com#P1> <http://ex.com#B> .\n"
-				+ "<http://ex.com#E> <http://ex.com#P2> <http://ex.com#F> .\n"
-				+ "<neo://query/bla> <http://ex.com#P1> <http://ex.com#B> .\n"
-				+ "<http://ex.com#A> <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2000/01/rdf-schema#Class> .\n";
-		StringReader r = new StringReader(data);
-		model = model.read(r, null, "N-TRIPLE");
-		return model;
-	}
+    public void testQueryModelStringInt_Query() {
+        Model m = modelWithClassesAndQuery();
+        Query q = new Query(m, 20);
+        assertEquals("The queries should be ignored when finding the classes from the context.", "bla", q.queryString);
+    }
 
-	public void testQueryModelStringInt_ThreeQuery() {
-		Model m = modelWithClassesAndThreeQueries();
-		try {
-			new Query(m, 20);
-		} catch (UnsupportedOperationException e) {
-			assertEquals("Multiple queries found in context. This is not supported yet!", e.getMessage());
-			return;
-		}
-		throw new junit.framework.AssertionFailedError("Expected exception was not thrown.");
+    public void testQueryModelStringInt_ThreeQuery() {
+        Model m = modelWithClassesAndThreeQueries();
+        try {
+            new Query(m, 20);
+        } catch (UnsupportedOperationException e) {
+            assertEquals("Multiple queries found in context. This is not supported yet!", e.getMessage());
+            return;
+        }
+        throw new junit.framework.AssertionFailedError("Expected exception was not thrown.");
 
-		//// For now, multiple queries are not supported, so testing exception rather
-		//// than expecting reuslts
+        //// For now, multiple queries are not supported, so testing exception rather
+        //// than expecting reuslts
 
-		// assertEquals("The queries should be ignored when finding the classes from the
-		// context.", Sets.newHashSet("bla", "blo", "blu"),
-		// Sets.newHashSet(q.queryStrings));
-	}
+        // assertEquals("The queries should be ignored when finding the classes from the
+        // context.", Sets.newHashSet("bla", "blo", "blu"),
+        // Sets.newHashSet(q.queryStrings));
+    }
 
-	public void testQueryModelStringInt_HashWithQueries() {
-		Model m1 = modelWithClasses();
-		Query q1 = new Query(m1, 20);
-		Model m2 = modelWithClassesAndQuery();
-		Query q2 = new Query(m2, 20);
-		assertTrue("The hash should not be modified by addition of statements including a query.",
-				q1.getContextHash().equals(q2.getContextHash()));
+    public void testQueryModelStringInt_HashWithQueries() {
+        Model m1 = modelWithClasses();
+        Query q1 = new Query(m1, 20);
+        Model m2 = modelWithClassesAndQuery();
+        Query q2 = new Query(m2, 20);
+        assertEquals("The hash should not be modified by addition of statements including a query.", q1.getContextHash(), q2.getContextHash());
 
-	}
+    }
 
-	// public void testExtractQuery() {
-	// Method method = Query.getDeclaredMethod("extractQueryStringFromContext",
-	// argClasses);
-	// method.setAccessible(true);
-	// return method.invoke(targetObject, argObjects);
-	// }
+    // public void testExtractQuery() {
+    // Method method = Query.getDeclaredMethod("extractQueryStringFromContext",
+    // argClasses);
+    // method.setAccessible(true);
+    // return method.invoke(targetObject, argObjects);
+    // }
 }
