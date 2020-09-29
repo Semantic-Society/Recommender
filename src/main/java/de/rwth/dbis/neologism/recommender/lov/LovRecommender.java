90c524eadc0879e5f4791273d2d7b81aecce8fc9
Johannes Lipp <johannes.lipp@fit.fraunhofer.de>
2020-09-29 13:49:37 +0200

    Code style improvements

--- src/main/java/de/rwth/dbis/neologism/recommender/lov/LovRecommender.java
+++ src/main/java/de/rwth/dbis/neologism/recommender/lov/LovRecommender.java
@@ -42,11 +42,11 @@ import java.util.logging.Logger;
 
 public class LovRecommender implements Recommender {
 
-	private List<String> labelsProperties = Collections.unmodifiableList(new ArrayList<>(
+	private final List<String> labelsProperties = Collections.unmodifiableList(new ArrayList<>(
 			Arrays.asList("http://www.w3.org/2000/01/rdf-schema#label", "vocabulary.http://purl.org/dc/terms/title",
 					"http://www.w3.org/2004/02/skos/core#", "localName.ngram")));
-	private final static String CREATOR = LovRecommender.class.getName();
-	private static final String address = "http://lov.okfn.org/dataset/lov/sparql";
+	private static final String CREATOR = LovRecommender.class.getName();
+	private static final String ADDRESS = "http://lov.okfn.org/dataset/lov/sparql";
 	/*
 	 * https://hc.apache.org/httpcomponents-client-ga/httpclient/apidocs/org/apache/
 	 * http/impl/client/HttpClientBuilder.html to check the list of parametrs to set
@@ -82,8 +82,97 @@ public class LovRecommender implements Recommender {
 			.build(new CacheLoader<OntologySearch, Recommendations>() {
 
 				@Override
-				public Recommendations load(OntologySearch key) throws Exception {
+				public Recommendations load(OntologySearch key) {
 					return recommendImplementation(key.keyword, key.numOfResults);
+				}
+
+				private Recommendations recommendImplementation(String queryString, int limit) {
+					Preconditions.checkNotNull(queryString);
+					Preconditions.checkArgument(queryString.length() > 0);
+					Preconditions.checkArgument(limit > 0);
+
+					URIBuilder b = new URIBuilder();
+					b.setScheme("http");
+					b.setHost("lov.okfn.org");
+					b.setPath("dataset/lov/api/v2/term/search");
+					b.addParameter("q", queryString);
+					b.addParameter("type", "class");
+					b.addParameter("page_size", limit + "");
+
+					// String request = "http://lov.okfn.org/dataset/lov/api/v2/term/search?q=" +
+					// queryString + "&type=class"
+					// + "&page_size=" + limit;
+
+					URI url;
+					try {
+						url = b.build();
+					} catch (URISyntaxException e1) {
+						throw new Error(e1);
+					}
+
+					HttpGet httpget = new HttpGet(url);
+
+					ResponseHandler<JsonLovTermSearch> responseHandler = response -> {
+						int status = response.getStatusLine().getStatusCode();
+						if (status == HttpStatus.SC_OK) {
+							HttpEntity entity = response.getEntity();
+							InputStream responseBody = entity.getContent();
+							return GSON.fromJson(
+									new JsonReader(new InputStreamReader(responseBody, StandardCharsets.UTF_8)),
+									JsonLovTermSearch.class);
+						} else {
+							Logger.getLogger(LovRecommender.class.getName()).severe("querying LOV failed for query " + url);
+							throw new ClientProtocolException("Unexpected response status: " + status);
+						}
+					};
+
+					JsonLovTermSearch item;
+					try {
+						item = HTTP_CLIENT.execute(httpget, responseHandler);
+					} catch (IOException e) {
+						throw new Error(e);
+					}
+
+					ArrayList<Result> resultsList = item.getResults();
+
+					List<Recommendation> recommendations = new ArrayList<>();
+					for (Result result : resultsList) {
+						// the ontology name is a prefix and not the URI
+						ArrayList<StringLiteral> labels = new ArrayList<>();
+						ArrayList<StringLiteral> comments = new ArrayList<>();
+
+						JsonObject highlights = result.getHighlight();
+						Set<Entry<String, JsonElement>> entrySet = highlights.entrySet();
+						for (Entry<String, JsonElement> entry : entrySet) {
+							Language language;
+							String[] splittedParts = entry.getKey().split("@");
+							if (splittedParts.length > 1 && splittedParts[1].length() == 2) {
+								language = Language.forLangCode(splittedParts[1]);
+							} else {
+								language = Language.EN;
+							}
+
+							JsonArray valueAsArray = entry.getValue().getAsJsonArray();
+							StringBuilder value = new StringBuilder();
+							for (JsonElement singleValue : valueAsArray) {
+								value.append(singleValue.getAsString());
+							}
+
+							if (labelsProperties.contains(splittedParts[0])) {
+								labels.add(new StringLiteral(language, value.toString()));
+							} else {
+								comments.add(new StringLiteral(language, value.toString()));
+							}
+
+						}
+
+						recommendations.add(
+								new Recommendation(result.getUri().get(0), result.getVocabulary_prefix().get(0), labels, comments));
+
+					}
+
+					return new Recommendations(recommendations, CREATOR);
+
 				}
 
 			});
@@ -103,94 +192,7 @@ public class LovRecommender implements Recommender {
 
 	}
 
-	private Recommendations recommendImplementation(String queryString, int limit) {
-		Preconditions.checkNotNull(queryString);
-		Preconditions.checkArgument(queryString.length() > 0);
-		Preconditions.checkArgument(limit > 0);
 
-		URIBuilder b = new URIBuilder();
-		b.setScheme("http");
-		b.setHost("lov.okfn.org");
-		b.setPath("dataset/lov/api/v2/term/search");
-		b.addParameter("q", queryString);
-		b.addParameter("type", "class");
-		b.addParameter("page_size", limit + "");
-
-		// String request = "http://lov.okfn.org/dataset/lov/api/v2/term/search?q=" +
-		// queryString + "&type=class"
-		// + "&page_size=" + limit;
-
-		URI url;
-		try {
-			url = b.build();
-		} catch (URISyntaxException e1) {
-			throw new Error(e1);
-		}
-
-		HttpGet httpget = new HttpGet(url);
-
-		ResponseHandler<JsonLovTermSearch> responseHandler = response -> {
-			int status = response.getStatusLine().getStatusCode();
-			if (status == HttpStatus.SC_OK) {
-				HttpEntity entity = response.getEntity();
-				InputStream responseBody = entity.getContent();
-				return GSON.fromJson(
-						new JsonReader(new InputStreamReader(responseBody, StandardCharsets.UTF_8)),
-						JsonLovTermSearch.class);
-			} else {
-				Logger.getLogger(LovRecommender.class.getName()).severe("querying LOV failed for query " + url);
-				throw new ClientProtocolException("Unexpected response status: " + status);
-			}
-		};
-
-		JsonLovTermSearch item;
-		try {
-			item = HTTP_CLIENT.execute(httpget, responseHandler);
-		} catch (IOException e) {
-			throw new Error(e);
-		}
-
-		ArrayList<Result> resultsList = item.getResults();
-
-		List<Recommendation> recommendations = new ArrayList<>();
-		for (Result result : resultsList) {
-			// the ontology name is a prefix and not the URI
-			ArrayList<StringLiteral> labels = new ArrayList<>();
-			ArrayList<StringLiteral> comments = new ArrayList<>();
-
-			JsonObject highlights = result.getHighlight();
-			Set<Entry<String, JsonElement>> entrySet = highlights.entrySet();
-			for (Entry<String, JsonElement> entry : entrySet) {
-				Language language;
-				String[] splittedParts = entry.getKey().split("@");
-				if (splittedParts.length > 1 && splittedParts[1].length() == 2) {
-					language = Language.forLangCode(splittedParts[1]);
-				} else {
-					language = Language.EN;
-				}
-
-				JsonArray valueAsArray = entry.getValue().getAsJsonArray();
-				StringBuilder value = new StringBuilder();
-				for (JsonElement singleValue : valueAsArray) {
-					value.append(singleValue.getAsString());
-				}
-
-				if (labelsProperties.contains(splittedParts[0])) {
-					labels.add(new StringLiteral(language, value.toString()));
-				} else {
-					comments.add(new StringLiteral(language, value.toString()));
-				}
-
-			}
-
-			recommendations.add(
-					new Recommendation(result.getUri().get(0), result.getVocabulary_prefix().get(0), labels, comments));
-
-		}
-
-		return new Recommendations(recommendations, CREATOR);
-
-	}
 
 	@Override
 	public PropertiesForClass getPropertiesForClass(PropertiesQuery q) {
@@ -205,7 +207,7 @@ public class LovRecommender implements Recommender {
 				+ "OPTIONAL{ ?p rdfs:comment ?comment }"
 				+ "FILTER ( (!(bound(?label) && bound(?comment))) || (lang(?comment) = lang(?label)))" + "}";
 
-		QueryExecution execution = QueryExecutionFactory.sparqlService(address, sparqlQuery);
+		QueryExecution execution = QueryExecutionFactory.sparqlService(ADDRESS, sparqlQuery);
 
 		ResultSet results = execution.execSelect();
 
